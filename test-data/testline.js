20c4df15fdf1fe5722d2319d5d4a15875daa8e80
awesselb <awesselb@materna.de>
2018-11-21 22:09:40 +0100

    New test cases written. Added test.js and test.vm for test purposes.

--- test-data/testline.js
+++ test-data/testline.js
@@ -1,8 +1,17 @@
 const testLines = {
+
   "single-line-single-quotes": "class='c d a 3 1 2'",
   "single-line-single-quotes-result": "class='1 2 3 a c d'",
+
   "single-line-double-quotes": 'class="c d a 3 1 2"',
   "single-line-double-quotes-result": 'class="1 2 3 a c d"',
+
+  "single-line-double-quotes-with-spaces": 'class="   c   d    a   3   1   2  "',
+
+  "multi-line-double-quotes" : "class=\"\n            c\n            d\n            a\n            3\n            1\n            2\n  \"",
+  "multi-line-single-quotes" : "class=\'\n            c\n            d\n            a\n            3\n            1\n            2\n  \'",
 };
+//TODO Save lines like errors
+
 
 module.exports = testLines;
--- test/test.js
+++ test/test.js
@@ -5,52 +5,45 @@ const expect = require('chai').expect;
 const Sorter = require('../src/sorter');
 const testLines = require('../test-data/testline');
 
-
 describe('Sort', function() {
   const sorter = new Sorter();
 
-  it('Convert single line double quotes class', function() {
+  it('Convert single line double quotes', function() {
     const result = sorter.sort(testLines['single-line-double-quotes']);
     expect(result).to.equal(testLines['single-line-double-quotes-result']);
   });
 
-  it('Convert single line single quotes class', function() {
+  it('Convert single line single quotes', function() {
     const result = sorter.sort(testLines['single-line-single-quotes']);
     expect(result).to.equal(testLines['single-line-single-quotes-result']);
   });
-/*
-  it('should convert double digits', function() {
-    const result = numFormatter(12);
-    expect(result).to.equal('12');
+
+  it('No unnecessary spaces', function () {
+    const result = sorter.sort(testLines['single-line-double-quotes-with-spaces']);
+    expect(result).to.equal(testLines['single-line-double-quotes-result']);
   });
 
-  it('should convert triple digits', function() {
-    const result = numFormatter(123);
-    expect(result).to.equal('123');
+  it('Convert multi line single quotes', function () {
+    const result = sorter.sort(testLines['multi-line-single-quotes']);
+    expect(result).to.equal(testLines['single-line-single-quotes-result']);
   });
 
-  it('should convert 4 digits', function() {
-    const result = numFormatter(1234);
-    expect(result).to.equal('1,234');
+  it('Convert multi line double quotes', function () {
+    const result = sorter.sort(testLines['multi-line-double-quotes']);
+    expect(result).to.equal(testLines['single-line-double-quotes-result']);
+  });
+});
+
+describe('ProcessContent', function() {
+  const sorter = new Sorter();
+
+  it('Convert single line double quotes class', function() {
+    const result = sorter.processContent(testLines['single-line-double-quotes'], '', true);
+    expect(result).to.equal(testLines['single-line-double-quotes-result']);
   });
 
-  it('should convert 5 digits', function() {
-    const result = numFormatter(12345);
-    expect(result).to.equal('12,345');
+  it('Convert single line single quotes class', function() {
+    const result = sorter.processContent(testLines['single-line-single-quotes'], '', true);
+    expect(result).to.equal(testLines['single-line-single-quotes-result']);
   });
-
-  it('should convert 6 digits', function() {
-    const result = numFormatter(123456);
-    expect(result).to.equal('123,456');
-  });
-
-  it('should convert 7 digits', function() {
-    const result = numFormatter(1234567);
-    expect(result).to.equal('1,234,567');
-  });
-
-  it('should convert 8 digits', function() {
-    const result = numFormatter(12345678);
-    expect(result).to.equal('12,345,678');
-  });*/
 });
